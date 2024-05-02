809f4a8d05faaa1f356def079066118a596ce4cc
Mansur Besleney <besleneymansur@gmail.com>
2024-05-02 21:49:02 +0300

    Game and Genre entities added. Database connections added

---
+++ GameStore.api/Entities/Game.cs
@@ -0,0 +1,12 @@
+﻿namespace GameStore.api.Entities
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public int GenreId { get; set; }
+        public Genre? Genre { get; set; } // In Entity Framework we need to define two properties if we need to apply one to one relation between our data models
+        public decimal Price { get; set; }
+        public DateOnly ReleaseDate { get; set; } 
+    }
+}
--- GameStore.api/Program.cs
+++ GameStore.api/Program.cs
@@ -1,6 +1,14 @@
+using GameStore.api.Data;
 using GameStore.api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = builder.Configuration.GetConnectionString("GameStore"); // The conString is coming grom appsettings.json file.
+                                                                        // Because of sqlLite there is no problem to store connection string at appsettings.json file
+                                                                       // If you use any database providers that includes any credentials at connection string you should use
+                                                                      // another way to store connection string like user secrets
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 var app = builder.Build();
 
 app.MapGamesEndpoints();

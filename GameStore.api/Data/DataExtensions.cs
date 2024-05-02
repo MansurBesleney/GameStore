961c562516606970e0504591c1c097b352bd4b96
Mansur Besleney <besleneymansur@gmail.com>
2024-05-02 22:29:03 +0300

    migration at first added

---
+++ GameStore.api/Data/DataExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.api.Data
+{
+    public static class DataExtensions
+    {
+        public static void MigrateDB(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+
+            var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
+
+            dbContext.Database.Migrate();
+        }
+    }
+}

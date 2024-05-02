d0148223399216672a80e748a2f45e6221e6c952
Mansur Besleney <besleneymansur@gmail.com>
2024-05-02 20:49:57 +0300

    RouteGroupBuilder added to GameStoreEndpoints

--- GameStore.api/Endpoints/GameStoreEndpoints.cs
+++ GameStore.api/Endpoints/GameStoreEndpoints.cs
@@ -42,13 +42,15 @@ namespace GameStore.api.Endpoints
 
             ];
 
-        public static WebApplication MapGamesEndpoints(this WebApplication app)
+        public static RouteGroupBuilder MapGamesEndpoints(this WebApplication app)
         {
+
+            var group = app.MapGroup("games");
             // GET /games
-            app.MapGet("games", () => games);
+            group.MapGet("/", () => games);
 
             // GET /game/1
-            app.MapGet("games/{id}", (int id) =>
+            group.MapGet("/{id}", (int id) =>
             {
                 GameDto? game = games.Find(game => game.Id == id);
 
@@ -63,7 +65,7 @@ namespace GameStore.api.Endpoints
             }).WithName(GetGameEndpointName);
 
             //POST /games
-            app.MapPost("games", (CreateGameDto newGame) =>
+            group.MapPost("/", (CreateGameDto newGame) =>
             {
                 GameDto game = new(
                 games.Count + 1,
@@ -79,7 +81,7 @@ namespace GameStore.api.Endpoints
             });
 
             //PUT games/1
-            app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) =>
+            group.MapPut("/{id}", (int id, UpdateGameDto updatedGame) =>
             {
                 int index = games.FindIndex(game => game.Id == id);
 
@@ -100,14 +102,14 @@ namespace GameStore.api.Endpoints
             });
 
             //DELETE games/1
-            app.MapDelete("games/{id}", (int id) =>
+            group.MapDelete("/{id}", (int id) =>
             {
                 games.RemoveAll(game => game.Id == id);
 
                 return Results.NoContent();
             });
 
-            return app;
+            return group;
         }
     }
 }

913a8f720dca28bc884dabd67283cab52f7a641d
Mansur Besleney <besleneymansur@gmail.com>
2024-05-04 00:54:49 +0300

    Genre data seed migration adeed

--- GameStore.api/Data/GameStoreContext.cs
+++ GameStore.api/Data/GameStoreContext.cs
@@ -1,4 +1,5 @@
 ﻿using GameStore.api.Entities;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.api.Data
@@ -7,5 +8,16 @@ namespace GameStore.api.Data
     {
         public DbSet<Game> Games => Set<Game>();
         public DbSet<Genre> Genres => Set<Genre>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Genre>().HasData(
+                new {Id = 1 , Name = "Fighting"},    
+                new {Id = 2 , Name = "RPG"},    
+                new {Id = 3 , Name = "Action&Adventure"},    
+                new {Id = 4 , Name = "Sports"},    
+                new {Id = 5 , Name = "Racing"}
+            );
+        }
     }
 }

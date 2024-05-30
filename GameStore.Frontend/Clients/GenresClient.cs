5e778fc63243c52702c33c41c4f4b62beefdd4f4
Mansur Besleney <besleneymansur@gmail.com>
2024-05-30 11:31:43 +0300

    genres dropdown list filled with data

---
+++ GameStore.Frontend/Clients/GenresClient.cs
@@ -0,0 +1,33 @@
+﻿using GameStore.Frontend.Models;
+
+namespace GameStore.Frontend.Clients
+{
+    public class GenresClient
+    {
+        private readonly Genre[] genres =
+        [
+                new(){
+                    Id = 1,
+                    Name = "FPS"
+                },
+                new(){
+                    Id = 2,
+                    Name = "RPG"
+                },
+                new(){
+                    Id = 3,
+                    Name = "Sports"
+                },
+                new(){
+                    Id = 4,
+                    Name = "Fighting"
+                },
+                new(){
+                    Id = 5,
+                    Name = "Racing"
+                },
+        ];
+
+        public Genre[] GetGenres() => genres;
+    }
+}
